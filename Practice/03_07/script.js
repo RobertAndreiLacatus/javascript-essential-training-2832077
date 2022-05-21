4a78a9d7073e1a280bda1e77ba0551252f1c74d8
RobertAndreiLacatus <100790743+RobertAndreiLacatus@users.noreply.github.com>
2022-05-21 18:35:34 +0300

    First object created in JavaScript

--- Practice/03_07/script.js
+++ Practice/03_07/script.js
@@ -1,9 +1,17 @@
-/**
- * Practice: Building objects
- *
- * - Create JavaScript objects based on objects in your current environment.
- * - Give each object an identifiable name.
- * - Create properties to describe the objects and set their values.
- * - Find an object that has another object inside of it to create a nested object.
- * - Test your objects in the browser console by accessing the entire object and its specific properties.
- */
+// My first object created
+
+
+const laptop ={
+    name :"Bussines Laptop",
+    companyproduct:"DELL",
+    RAMnumber:16,
+    clap:{
+        on:true,
+        off:false,
+    }
+}
+
+
+console.log("The name of the object:", laptop);
+console.log("Show me the Company which products this laptop:", laptop.companyproduct);
+console.log("Is this laptop open? ", laptop.clap.on);
